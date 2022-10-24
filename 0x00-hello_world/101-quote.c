c3b41c6aa9369626609b9c8b367a3c2edf9763ad
Schiphtan <tuyumvire@gmail.com>
2022-10-24 01:46:12 -0700

    Real 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,17 +3,12 @@
 /**
  * main - Entry point
  *
- * Description: Will print string
- *
  * Return 1 (Successful)
  */
-
 int main(void)
 {
-	char str1[] = "and that piece of art is useful\" - Dora Korpar,
-	     2015-10-19\n";
+	char str1[] = "and that piece of art is useful\" - Dora Korpa, 2015-10-19\n";
 
 	write(2, str1, 59);
-
 	return (1);
 }
