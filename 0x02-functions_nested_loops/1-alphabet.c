f6e66014c53a7286cdad6d4ce69858688beab60b
Schiphtan <tuyumvire@gmail.com>
2022-10-27 08:42:32 -0700

    alphabet a-z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Program will print alphabets
+ *
+ */
+void print_alphabet(void)
+{
+	char az;
+
+	for (az = 'a'; az <= 'z'; az++)
+		_putchar(az);
+
+	_putchar('\n');
+}
