cfb051cbeefa6a74455d74ea4d3e07dd8e9e3b94
staner schi <tuyumvire@gmail.com>
2023-07-17 20:50:50 -0700

    Print the line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "h"
+
+/**
+  * print_line - print line
+  *@n: number of times the character _ shoud be print
+  * Return: Void
+  */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_')
+		}
+		_putchar('\n');
+	}
+}
