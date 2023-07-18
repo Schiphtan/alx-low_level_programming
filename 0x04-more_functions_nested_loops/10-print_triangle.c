e617467ea5cf484a4f53051047a54b6e2368a7c6
staner schi <tuyumvire@gmail.com>
2023-07-18 03:08:58 -0700

    print trangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,7 +7,7 @@ void print_triangle(int size)
 {
 	if (size <= 0)
 	{
-		_putchar(' ')
+		_putchar(' ');
 	} else
 	{
 		int i, j;
@@ -18,7 +18,7 @@ void print_triangle(int size)
 			{
 				_putchar(' ');
 			}
-			for (j = 1; j <= i, j++)
+			for (j = 1; j <= i; j++)
 			{
 				_putchar('#');
 			}
