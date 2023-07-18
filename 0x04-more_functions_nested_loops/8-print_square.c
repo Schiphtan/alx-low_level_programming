3932ec007b16c7d7b5e7550f56cbcc6f6c1139a0
staner schi <tuyumvire@gmail.com>
2023-07-18 01:37:26 -0700

    It prints the square.

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,20 +2,21 @@
 
 /**
   * print_square - prints a square, followed by a new line
-  * @n: size of square
+  * @size: size of square
   */
 
-void print_square(int n)
+void print_square(int size)
 {
-	if (n <= 0)
+	if (size <= 0)
+	{
 		_putchar('\n');
-	else
+	} else
 	{
 		int i, j;
 
-		for (i = 0; i < n; i++)
+		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j < n; j++)
+			for (j = 0; j < size; j++)
 			{
 				_putchar('#');
 			}
