b09ce7cd1754235db9c2585d5da05355c0af8498
staner schi <tuyumvire@gmail.com>
2023-07-25 00:49:19 -0700

    print an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,19 +8,20 @@
 
 void print_number(int n)
 {
-	unsigned int n1;
-
-	n1 = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n1 = -n;
+		n = -n;
 	}
 
-	if (n1 / 10 != 0)
+	if (n / 10 == 0)
 	{
-		print_number(n1 / 10);
+		_putchar(n + '0');
 	}
-	_putchar((n1 % 1) + '0');
+	else
+	{
+	print_number(n / 10);
+	_putchar(n % 10 + '0');
+	}
 }
