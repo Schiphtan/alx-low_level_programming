a8a5bfd56402e1cbbd0e2e342d14d31a572b14dc
staner schi <tuyumvire@gmail.com>
2023-07-25 02:25:16 -0700

    prints a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 
 /**
-  * print_buffer - prints buffer
-  * @b: buffer
-  * @size: size
-  * Return: void
-  */
+ * print_buffer - prints buffer
+ * @b: buffer
+ * @size: size
+ * Return: void
+ */
+
 void print_buffer(char *b, int size)
 {
 	int o, j, i;
@@ -20,14 +21,14 @@ void print_buffer(char *b, int size)
 	}
 	while (o < size)
 	{
-		j = size - o < 10 ? size : 10;
-		printf("%08x:", o);
+		j = size - o < 10 ? size - o : 10;
+		printf("%08x: ", o);
 		for (i = 0; i < 10; i++)
 		{
 			if (i < j)
 				printf("%02x", *(b + o + i));
 			else
-				printf(" ");
+				printf("  ");
 			if (i % 2)
 			{
 				printf(" ");
@@ -35,15 +36,16 @@ void print_buffer(char *b, int size)
 		}
 		for (i = 0; i < j; i++)
 		{
-		int c = *(b + o + i);
+			int c = *(b + o + i);
 
-		if (c < 32 || c > 132)
-		{
-			c = '.';
-		}
-		printf("%c", c);
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
 		}
 		printf("\n");
 		o += 10;
 	}
 }
+
